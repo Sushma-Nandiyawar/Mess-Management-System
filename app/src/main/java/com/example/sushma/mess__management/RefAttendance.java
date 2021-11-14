e046cca595dc194263c09e6f5ab82d800c5701c2
Sushma N <sushmanandiyawar5621@gmail.com>
2021-11-14 18:37:04 +0530

    pushing code to git repo

---
+++ app/src/main/java/com/example/sushma/mess__management/RefAttendance.java
@@ -0,0 +1,13 @@
+package com.example.sushma.mess__management;
+
+
+public class RefAttendance {
+
+    String phone;
+    String platesConsumed;
+    String platesremain;
+    String error;
+    int daysremain;
+
+    RefAttendance(){};
+}
